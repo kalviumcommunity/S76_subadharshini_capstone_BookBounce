df658d8654baef7ed22040d2e3de34439e25f936
suba dharshini <subakarthick2005@gmail.com>
2025-05-09 17:40:16 +0530

    implemented authetication

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,19 +1,15 @@
-// src/components/Navbar.jsx
 import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav className="bg-green-500 text-white px-6 py-4 shadow-md">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="text-2xl font-bold">BookBoune</div>
-        <ul className="flex space-x-6 text-lg">
-          <li><a href="#" className="hover:text-gray-300">Home</a></li>
-          <li><a href="#" className="hover:text-gray-300">MyBooks</a></li>
-          <li><a href="#" className="hover:text-gray-300">Swap</a></li>
-          <li><a href="#" className="hover:text-gray-300">Login</a></li>
-          <li><a href="#" className="hover:text-gray-300">About Us</a></li>
-        </ul>
-      </div>
+    <nav className="bg-green-400 text-black py-4 px-6 rounded-full mx-auto min-w-[500px] mt-4 flex items-center justify-between">
+      <div className="text-xl font-semibold ml-4">Book Bounce</div>
+      <ul className="flex space-x-6 mr-4">
+        <li><a href="#" className="hover:underline">Home</a></li>
+        <li><a href="#" className="hover:underline">MyBooks</a></li>
+        <li><a href="#" className="hover:underline">Login</a></li>
+        <li><a href="#" className="hover:underline">About</a></li>
+      </ul>
     </nav>
   );
 };
--- client/src/routes/Routes.js
+++ client/src/routes/Routes.js
@@ -1,5 +1,9 @@
 import Home from "../pages/Home";
+import Signup from "../pages/SignUp";
+import Login from "../pages/Login";
 
 export{
-    Home
+    Home,
+    Signup,
+    Login
 }
